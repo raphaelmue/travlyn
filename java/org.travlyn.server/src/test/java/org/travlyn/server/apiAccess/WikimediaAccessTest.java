a44f83174c8e06cbdc23987d6121700c2d15ba86
Joshua Schulz <joshua.schulz@sap.com>
2020-01-26 15:36:39 +0100

    issue#14: little fix for unit test

--- java/org.travlyn.server/src/test/java/org/travlyn/server/apiAccess/WikimediaAccessTest.java
+++ java/org.travlyn.server/src/test/java/org/travlyn/server/apiAccess/WikimediaAccessTest.java
@@ -1,5 +1,6 @@
 package org.travlyn.server.apiAccess;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.Tag;
 
@@ -8,8 +9,8 @@ public class WikimediaAccessTest {
 
     @Test
     public void testIntroCall(){
-        WikimediaRequest request = new WikimediaRequest("Düsseldorf");
+        WikimediaRequest request = new WikimediaRequest("Wesel");
         String result = request.getIntro();
-        System.out.println(result);
+        Assert.assertEquals("\"Wesel (German pronunciation: [ˈveːzl̩]) is a city in North Rhine-Westphalia, Germany. It is the capital of the Wesel district.\\n\"",result);
     }
 }
