a122c78491dd9640c252e7b4f70520b7fff5fdf6
Raphael Müßeler <raphael@muesseler.de>
2020-04-13 14:39:27 +0200

    issue #47: added chapter Controller Interfaces

---
+++ docs/work/code/CityApi.java
@@ -0,0 +1,19 @@
+@Api(value = "city")
+public interface CityApi {
+
+  @ApiOperation(
+    value = "Get City by search term",
+    nickname = "getCity",
+    notes = "",
+    response = City.class,
+    authorizations = {@Authorization(value = "ApiKeyAuth")},
+    tags = {"city"})
+  @ApiResponses(value = {
+    @ApiResponse(code = 200, message = "successful operation", response = City.class),
+    @ApiResponse(code = 401, message = "You are not authorized to perform this action")})
+  @GetMapping(
+    value = "/city",
+    produces = {"application/json"})
+  ResponseEntity<City> getCity(@ApiParam(value = "Name of the city that should be searched for", required = true, defaultValue = "", example = "Düsseldorf") @Valid @RequestParam(value = "query") String query);
+
+}
