87353139415b290a9340e6ae9106945b2d2d57ff
Joshua Schulz <joshua.schulz@sap.com>
2020-04-18 16:12:01 +0200

    issue #56: fixed directions unit test

--- java/org.travlyn.server/src/test/java/org/travlyn/server/externalapi/access/OpenRouteDirectionsTest.java
+++ java/org.travlyn.server/src/test/java/org/travlyn/server/externalapi/access/OpenRouteDirectionsTest.java
@@ -1,13 +1,14 @@
 package org.travlyn.server.externalapi.access;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.travlyn.shared.model.api.ExecutionInfo;
 import org.travlyn.shared.model.api.Stop;
-import org.travlyn.shared.model.db.StopEntity;
+import org.travlyn.shared.model.api.User;
 import org.travlyn.shared.model.db.TripEntity;
+import org.travlyn.shared.model.db.TripStopEntity;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,12 +17,14 @@ public class OpenRouteDirectionsTest {
 
     @Test
     public void testTripRouteCall(){
-        Stop stop = new Stop().setLatitude(51.517021).setLongitude(-0.117408);
-        Set<StopEntity> set = new HashSet<>();
-        set.add(stop.toEntity());
-        //TripEntity entity = new TripEntity().setStops(set);
-        OpenRouteTripDirectionRequest tripRequest = new OpenRouteTripDirectionRequest(51.507,-0.1275, null,false);
+        Stop stop = new Stop().setLatitude(-0.117408).setLongitude(51.517021);
+        Set<TripStopEntity> set = new HashSet<>();
+        set.add(new TripStopEntity().setStop(stop.toEntity()));
+        User user = new User().email("mail@mail.com").name("name").id(1);
+        TripEntity entity = new TripEntity().setStops(set).setUser(user.toEntity());
+        OpenRouteTripDirectionRequest tripRequest = new OpenRouteTripDirectionRequest(51.507,-0.1275, entity.toDataTransferObject(),false);
         ExecutionInfo info = tripRequest.getResult();
-        System.out.println("Hallo!");
+        Assertions.assertEquals(85,info.getWaypoints().size());
+        Assertions.assertEquals(1.5843,info.getDistance());
     }
 }
